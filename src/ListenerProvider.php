b91208e74311446ac788391ac2f3a15b92b3197d
Quang-Minh <quang-minh@emerya.fr>
2021-05-25 20:07:42 +0200

    Listeners are now shared and no longer only tied to the listener provider.

--- src/ListenerProvider.php
+++ src/ListenerProvider.php
@@ -58,7 +58,7 @@ class ListenerProvider extends ComponentBasedObject implements ListenerProviderI
 
         foreach ($this->listenerGroups[$event->getName()]->getListeners() as $listenerClassName) {
             // Retrieve the listener instance.
-            $instance = $this->getComponentByClassName($listenerClassName, true);
+            $instance = static::getSharedComponentByClassName($listenerClassName, true);
 
             if (empty($instance)) {
                 // Failed to load the listener.
